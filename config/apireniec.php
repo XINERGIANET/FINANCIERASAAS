9e35503bafe25169ab4cb16fc24964c042a5ea06
USERcristhian01 <141697738+USERcristhian01@users.noreply.github.com>
2026-07-03 18:14:33 -0500

    dniyruc

--- config/apireniec.php
+++ config/apireniec.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-	'url' => env('APIRENIEC_URL'),
-	'token' => env('APIRENIEC_TOKEN')
+    'url' => env('APIRENIEC_URL', 'https://api.json.pe/api/dni'),
+    'ruc_url' => env('APIRENIEC_URL_RUC', 'https://api.json.pe/api/ruc'),
+    'key' => env('APIRENIEC_KEY', ''),
 ];
