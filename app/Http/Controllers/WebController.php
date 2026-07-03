ef83eddc7e2d6542270e06b2ffa6776b902ca7b5
USERcristhian01 <141697738+USERcristhian01@users.noreply.github.com>
2026-07-03 18:14:33 -0500

    dniyruc

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -864,18 +864,21 @@ class WebController extends Controller
     }
 
     public function apiReniec(Request $request){
-        $response = Http::get('https://api.perudevs.com/api/v1/dni/simple', [
-            'document' => $request->dni,
-            'key' => env('APIRENIEC_KEY'),
-        ]);
+        $response = Http::withToken((string) config('apireniec.key'))
+            ->timeout(15)
+            ->post((string) config('apireniec.url'), [
+                'dni' => $request->dni,
+            ]);
 
-        $data = $response->json();
+        $data = (array) $response->json();
+        $estado = (bool) ($data['success'] ?? false);
+        $resultado = (array) ($data['data'] ?? []);
 
-        if($response->successful() && isset($data['estado']) && $data['estado'] === true && !empty($data['resultado']['nombre_completo'])){
+        if($response->successful() && $estado === true && !empty($resultado['nombre_completo'])){
 
             return response()->json([
                 'status' => true,
-                'name' => $data['resultado']['nombre_completo']
+                'name' => $resultado['nombre_completo']
             ]);
 
         }else{
